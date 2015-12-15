287259e06a459050ad051e88508e1a781705cd0f
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2015-12-15 10:28:51 +0000

    tweak rearrangements as per @johnmccabe's observation on mailing list (14 dec)

--- rearrange-incubator.sh
+++ rearrange-incubator.sh
@@ -7,27 +7,38 @@ set -x
 pushd incubator-brooklyn
 
 git reset --hard
+git checkout master
+git pull
+git branch -D reorg || true
+git checkout -b reorg
 git clean -df
+
 
 for x in $PROJS ; do mkdir -p brooklyn-$x ; done
 mkdir -p brooklyn
 
 for x in `ls | grep brooklyn` ; do cp .gitattributes .gitignore LICENSE NOTICE README.md $x ; done
 
-mv usage/all usage/dist usage/scripts usage/downstream-parent usage/archetypes release brooklyn-dist
+mv usage/all usage/dist usage/scripts usage/downstream-parent usage/archetypes release brooklyn-dist/
 
 mv usage/cli brooklyn-server/server-cli
-mv parent api core policy utils camp locations usage/rest-* usage/logback-* usage/launcher usage/qa usage/test-* storage karaf  brooklyn-server
+mkdir brooklyn-server/software
+mv software/base software/winrm brooklyn-server/software/
+mkdir brooklyn-server/logging
+mv usage/logback-* brooklyn-server/logging/
+mkdir brooklyn-server/rest
+mv usage/rest-* brooklyn-server/rest/
+mv parent api core policy utils camp locations usage/launcher usage/test-* storage karaf  brooklyn-server/
 mv usage/camp brooklyn-server/camp/camp-brooklyn
+
+mv usage/qa software examples sandbox brooklyn-library/
 
 mv usage/jsgui/* brooklyn-ui
 cat usage/jsgui/.gitignore >> brooklyn-ui/.gitignore
-rm -rf usage
+rm -r usage
 
-mv software examples sandbox brooklyn-library
-
-mv docs/{*,.gitignore} brooklyn-docs
-rm -rf docs
+mv docs/{*,.gitignore} brooklyn-docs/
+rm -r docs
 
 popd
 
