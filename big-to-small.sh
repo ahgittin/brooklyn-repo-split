3f1b31ae34a635ceb78ef9075fd832fe39b840a8
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2015-12-16 19:57:59 +0000

    add comment on big-to-small limitation
    explains why we weren't getting the reduction we expected, but now we should be good...

--- big-to-small.sh
+++ big-to-small.sh
@@ -2,6 +2,8 @@
 # lists files in history in size order, in big-to-small.gen.txt
 # capped at 200 of the biggest blobs (generating about 3000 of the biggest size files in history)
 
+# however this does not show the former names of the blobs so if the same file is in multiple times this script will not currently show you all instances!
+
 pushd incubator-brooklyn
 git rev-list --objects --all | sort -k 2 > ../TMP-allfileshas.txt
 git gc && git verify-pack -v .git/objects/pack/pack-*.idx | egrep "^\w+ blob\W+[0-9]+ [0-9]+ [0-9]+$" | sort -k 3 -n -r | head -200 > ../TMP-bigobjects.txt
