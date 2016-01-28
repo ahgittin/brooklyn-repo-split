ed32bb3c338095948a440d30aead7614b8a02a04
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2016-01-28 14:08:36 +0000

    Ensure incubator project is clean before beginning.

--- all.sh
+++ all.sh
@@ -5,24 +5,26 @@ set -e
 
 . env.sh
 
-# ensure branches are all checked out
+# ensure branches are all checked out, updated, and restored
 ( cd incubator-brooklyn && git fetch )
 ( cd incubator-brooklyn && for branch in ${branches}; do [ -z "$( git branch --list ${branch} )" ] && git branch --track ${branch} origin/${branch} || true; done )
 ( cd incubator-brooklyn && git checkout master && git reset --hard origin/master )
+# use the "reorg" branch for cleaned history, but first update everything from origin
+( cd incubator-brooklyn && git pull && ( git branch -D reorg || true) && 
+  ( for x in $(git branch | cut -c 3-) ; do git checkout $x ; git reset --hard origin/$x ; done ) &&
+  git checkout master && git checkout -b reorg )
 
-# now restructure *in the old repo*
 
-# use the "reorg" branch when we clean history
-( cd incubator-brooklyn && git pull && ( git branch -D reorg || true) && git checkout -b reorg )
-# REARRANGE has been done, but if we needed to do it again we could again use this command, which will re-create the above reorg branch
+# REARRANGE has been done, so this should now be skipped
 # ./1-rearrange-incubator.sh
 
+# this is applied to all branches, local repo only; 
+# should NOT be pushed, it's just the basis for the splitting
 ./2-clean-history.sh
-# should affect all branches, but should NOT be pushed
-# it's just the basis for the splitting
 
+# now create the list of files relevant to each subproject
 ./3-create-full-whitelists.sh
-
 
 # make the new repos, in new-repos/
 ./4-make-new-repos.sh
+
